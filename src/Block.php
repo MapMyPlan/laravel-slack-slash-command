908f473d517e0f7d77a24b66640c7002d35006d5
Rubens Peculis <rubens@mapmyplan.com>
2020-03-04 15:37:48 +1100

    Added block create

--- src/Block.php
+++ src/Block.php
@@ -3,15 +3,64 @@
 
 namespace MapMyPlan\SlashCommand;
 
-
 class Block
 {
     protected $type = 'section';
-    protected $blockId;
+    protected $block_id;
     /** @var BlockText */
     protected $text;
     /** @var BlockAccessory */
     protected $accessory;
 
+    public static function create($type)
+    {
+        return new Block($type);
+    }
 
+    public function __construct(string $type)
+    {
+        $this->type = $type;
+    }
+
+    public function setBlockId(string $blockId)
+    {
+        $this->block_id = $blockId;
+        return $this;
+    }
+
+    public function setText($text)
+    {
+        if (is_a($text, BlockText::class)) {
+            $this->text = $text;
+        } elseif (is_array($text)) {
+            if (isset($text['type']) && isset($text['text'])) {
+                $this->text = BlockText::create($text['type'], $text['text']);
+            } elseif (isset($text[0]) && isset($text[1])) {
+                $this->text = BlockText::create($text[0], $text[1]);
+            } elseif (isset($text[0])) {
+                $this->text = BlockText::create('mrkdwn', $text[0]);
+            } else {
+                $this->text = BlockText::create('mkrdwn', '');
+            }
+        } elseif (is_string($text)) {
+            $this->text = BlockText::create('mrkdwn', $text);
+        } else {
+            $this->text = BlockText::create('mkrdwn', '');
+        }
+        return $this;
+    }
+
+    public function setAccessory($accessory)
+    {
+        if (is_a($accessory, BlockAccessory::class)) {
+            $this->accessory = $accessory;
+        } else {
+            $this->accessory = BlockAccessory::create(
+                $accessory['type'],
+                $accessory['image_url'],
+                isset($accessory['alt_text']) ? $accessory['alt_text'] : ''
+            );
+        }
+        return $this;
+    }
 }
